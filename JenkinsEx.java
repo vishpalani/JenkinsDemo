1d2847beb785bccedece9db2390ccd47ffdb7b61
vishpalani <135815939+vishpalani@users.noreply.github.com>
2023-07-14 15:56:34 +0530

    Jenkins Sample  Example

---
+++ JenkinsEx.java
@@ -0,0 +1,8 @@
+public class JenkinsEx{
+	public static void main(String[] args) {
+		System.out.println("hiiii");
+		System.out.println("hello");
+		System.out.println("welcome");
+		System.out.println("entry");
+}
+}
